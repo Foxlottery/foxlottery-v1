a213ebdce960be80f65148c8ccafe356c7b28b79
PeterTakahashi <seiya4@icloud.com>
2022-06-05 00:49:44 +0900

    indexを加える

--- contracts/TokenTimedRandomSendContract.sol
+++ contracts/TokenTimedRandomSendContract.sol
@@ -11,26 +11,13 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 contract TokenTimedRandomSendContract is VRFConsumerBase, Ownable {
     using SafeMath for uint256;
 
+    // constant
     string public name;
     string public symbol;
     uint public cycleTimestamp;
     uint public closeTimestamp;
     IERC20 public erc20; // erc20 token used for lottery
-    uint public ticketPrice;
-    uint public ticketLastId = 0;
     bool public isOnlyOwner;
-
-    // ticket
-    mapping(uint => uint) public ticketCount;
-    mapping(uint => uint) public ticketLastNumber;
-    mapping(address => uint[]) private ticketIds;
-    mapping(uint => address) public ticketOwner;
-    mapping(address => mapping(uint => bool)) public ticketIdMap;
-    mapping(uint => uint) public ticketBoughtAt;
-
-    // participant
-    uint public participantCount = 0;
-    mapping(address => bool) public isParticipated;
 
     // Chainlink VRF Variables
     bytes32 public keyHash;
@@ -40,6 +27,22 @@ contract TokenTimedRandomSendContract is VRFConsumerBase, Ownable {
     mapping(uint256 => uint256) public randomMap; // maps a eventCount to a random number
     mapping(bytes32 => uint256) public requestMap; // maps a requestId to a eventCount
 
+    uint public index = 1; // event count
+
+    // ticket config
+    mapping(uint => uint) public _ticketPrice;
+
+    // ticket
+    mapping(uint => uint) public _ticketLastId;
+    mapping(uint => mapping(uint => uint)) private _ticketCount;
+    mapping(uint => mapping(uint => uint)) private _ticketLastNumber;
+    mapping(uint => mapping(address => uint[])) private _ticketIds;
+    mapping(uint => mapping(uint => uint)) private _ticketReceivedAt;
+
+    // participant
+    mapping(uint => uint) private _participantCount;
+    mapping(uint => mapping(address => bool)) public _isParticipated;
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -48,7 +51,7 @@ contract TokenTimedRandomSendContract is VRFConsumerBase, Ownable {
         address _link,
         address _coordinator, 
         bytes32 _keyHash,
-        uint _ticketPrice,
+        uint __ticketPrice,
         bool _isOnlyOwner)
     VRFConsumerBase(_coordinator, _link)
     {
@@ -57,7 +60,7 @@ contract TokenTimedRandomSendContract is VRFConsumerBase, Ownable {
         cycleTimestamp = _cycleTimestamp;
         closeTimestamp = block.timestamp + _cycleTimestamp;
         erc20 = _erc20;
-        ticketPrice = _ticketPrice;
+        _ticketPrice[index] = __ticketPrice;
         isOnlyOwner = _isOnlyOwner;
         
         // Chainlink setters
@@ -79,56 +82,54 @@ contract TokenTimedRandomSendContract is VRFConsumerBase, Ownable {
 
      /**
     * @notice buy lottery ticket
-    * @param _ticketCount Amount of lottery tickets
+    * @param __ticketCount Amount of lottery tickets
     * @dev When you buy a lottery ticket, you lock the funds in a smart contract wallet.
     * onlyOwnerable オーナーだけがチケットを購入できるようにする or 誰でもチケットを購入できるようにする
     */
-    function buyTicket(uint256 _ticketCount) public payable onlyBeforeCloseTimestamp onlyOwnerable {
-        uint tokenAmount = _ticketCount * ticketPrice;
+    function buyTicket(uint256 __ticketCount) public payable onlyBeforeCloseTimestamp onlyOwnerable {
+        uint tokenAmount = __ticketCount * _ticketPrice[index];
         require(erc20.balanceOf(msg.sender) >= tokenAmount, "TokenTimedRandomSendContract: Not enough erc20 tokens.");
 
         // ticket
-        ticketLastId++;
-        ticketCount[ticketLastId] = _ticketCount;
-        ticketLastNumber[ticketLastId] = ticketLastNumber[ticketLastId - 1] + _ticketCount;
+        _ticketLastId[index]++;
+        _ticketCount[index][_ticketLastId[index]] = __ticketCount;
+        _ticketLastNumber[index][_ticketLastId[index]] = _ticketLastNumber[index][_ticketLastId[index] - 1] + __ticketCount;
 
-        ticketIds[msg.sender].push(ticketLastId);
-        ticketOwner[ticketLastId] = msg.sender;
-        ticketBoughtAt[ticketLastId] = block.timestamp;
+        _ticketIds[index][msg.sender].push(_ticketLastId[index]);
+        _ticketReceivedAt[index][_ticketLastId[index]] = block.timestamp;
 
-        addParticipantCount();
+        addParticipantCount(msg.sender);
 
         // Lock the Lottery in the contract
         erc20.transferFrom(msg.sender, address(this), tokenAmount);
     }
 
     function sendTicket(uint ticketIdsIndex, address to) public onlyOwner {
-        uint ticketId = getTicketIdFromTicketIds(ticketIdsIndex);
-        console.log(ticketId);
-        // // remove
-        // ticketIds[msg.sender][ticketIdsIndex] = ticketIds[msg.sender][ticketIds[msg.sender].length - 1];
-        // ticketIds[msg.sender].pop();
-        // delete ticketOwner[ticketId];
+        uint ticketId = ticketIdFromTicketIds(ticketIdsIndex);
 
-        // // add
-        // ticketIds[to].push(ticketId);
-        // ticketOwner[ticketId] = to;
+        // remove
+        _ticketIds[index][msg.sender][ticketIdsIndex] = _ticketIds[index][msg.sender][_ticketIds[index][msg.sender].length - 1];
+        _ticketIds[index][msg.sender].pop();
+
+        // add
+        _ticketIds[index][to].push(ticketId);
+        addParticipantCount(to);
     }
 
     /**
     * @notice add participant count
     */
-    function addParticipantCount() internal {
-        if (!isParticipated[msg.sender]) {
+    function addParticipantCount(address user) internal {
+        if (!_isParticipated[index][user]) {
             // lottery purchaser add participants
-            participantCount += 1;
+            _participantCount[index] += 1;
         }
 
-        isParticipated[msg.sender] = true;
+        _isParticipated[index][user] = true;
     }
 
-    function getTicketIds(address user) public view returns(uint[] memory) {
-        return ticketIds[user];
+    function ticketIds(uint _index, address user) public view returns(uint[] memory) {
+        return _ticketIds[_index][user];
     }
 
     /**
@@ -150,14 +151,36 @@ contract TokenTimedRandomSendContract is VRFConsumerBase, Ownable {
         _;
     }
 
-    modifier hasTicket(uint ticketIdsIndex) {
-        uint ticketId = getTicketIdFromTicketIds(ticketIdsIndex);
-        require(ticketOwner[ticketLastId] == msg.sender, "TokenTimedRandomSendContract: require have ticket");
-        _;
+
+    function ticketIdFromTicketIds(uint _ticketIdsIndex) internal view returns(uint) {
+        return _ticketIds[index][msg.sender][_ticketIdsIndex];
     }
 
-    function getTicketIdFromTicketIds(uint ticketIdsIndex) internal view returns(uint) {
-        console.log(ticketIds[msg.sender].length);
-        return ticketIds[msg.sender][ticketIdsIndex];
+    function ticketPrice(uint _index) public view returns(uint) {
+        return _ticketPrice[_index];
+    }
+
+    function ticketLastId(uint _index) public view returns(uint) {
+        return _ticketLastId[_index];
+    }
+
+    function ticketCount(uint _index, uint ticketId) public view returns(uint) {
+        return _ticketCount[_index][ticketId];
+    }
+
+    function ticketLastNumber(uint _index, uint ticketId) public view returns(uint) {
+        return _ticketLastNumber[_index][ticketId];
+    }
+
+    function ticketReceivedAt(uint _index, uint ticketId) public view returns(uint) {
+        return _ticketReceivedAt[_index][ticketId];
+    }
+
+    function participantCount(uint _index) public view returns(uint) {
+        return _participantCount[_index];
+    }
+
+    function isParticipated(uint _index, address user) public view returns(bool) {
+        return _isParticipated[_index][user];
     }
 }
--- test/TokenTimedRandomSendContract.ts
+++ test/TokenTimedRandomSendContract.ts
@@ -7,6 +7,7 @@ const _keyHash =
   "0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311";
 const _ticketPrice = String(10 ** 19);
 const _cycleTimestamp = 120;
+const index = 1;
 
 async function mintAndApprove(
   lotteryERC20: any,
@@ -69,19 +70,22 @@ describe("TokenTimedRandomSendContract", function () {
       const name = await this.weeklyLottery.name();
       const symbol = await this.weeklyLottery.symbol();
       const cycleTimestamp = await this.weeklyLottery.cycleTimestamp();
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const keyHash = await this.weeklyLottery.keyHash();
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         this.signers[0].address
       );
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       const isOnlyOwner = await this.weeklyLottery.isOnlyOwner();
 
       expect(name).to.equal("weeklyLottery");
@@ -100,7 +104,7 @@ describe("TokenTimedRandomSendContract", function () {
 
     it("Owner should buy ticket", async function () {
       const user = this.signers[0];
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -113,44 +117,48 @@ describe("TokenTimedRandomSendContract", function () {
       );
 
       // ticketLastIdが追加されていること
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       expect(ticketLastId).to.equal("1");
 
       // ticketLastNumberがあること
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       expect(ticketLastNumber).to.equal("3");
 
       // ticketCountが追加されていること
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       expect(ticketCount).to.equal("3");
 
       // isParticipatedがtrueに更新されていること
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         user.address
       );
       expect(isParticipated).to.equal(true);
 
       // participantCountが更新されていること
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       expect(participantCount).to.equal("1");
 
       // ticketIds
-      const ticketIds = await this.weeklyLottery.getTicketIds(user.address);
+      const ticketIds = await this.weeklyLottery.ticketIds(index, user.address);
       expect(ticketIds[0]).to.equal("1");
 
-      // ticketBoughtAt
-      const ticketBoughtAt = await this.weeklyLottery.ticketBoughtAt(
+      // ticketReceivedAt
+      const ticketReceivedAt = await this.weeklyLottery.ticketReceivedAt(
+        index,
         ticketLastId
       );
-      expect(ticketBoughtAt <= new Date().getTime()).to.equal(true);
+      expect(ticketReceivedAt <= new Date().getTime()).to.equal(true);
     });
 
     it("Two user should buy ticket", async function () {
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -173,48 +181,48 @@ describe("TokenTimedRandomSendContract", function () {
       );
 
       // ticketLastIdが追加されていること
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       expect(ticketLastId).to.equal("2");
 
       // ticketLastNumberがあること
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       expect(ticketLastNumber).to.equal("6");
 
       // ticketCountが追加されていること
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       expect(ticketCount).to.equal("3");
 
       // isParticipatedがtrueに更新されていること
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         user.address
       );
       expect(isParticipated).to.equal(true);
 
       // participantCountが更新されていること
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       expect(participantCount).to.equal("2");
 
       // ticketIds
-      const ticketIds = await this.weeklyLottery.getTicketIds(user.address);
+      const ticketIds = await this.weeklyLottery.ticketIds(index, user.address);
       expect(ticketIds[0]).to.equal("2");
 
-      // ticketOwner
-      const ticketOwner = await this.weeklyLottery.ticketOwner(ticketLastId);
-      expect(ticketOwner).to.equal(user.address);
-
-      // ticketBoughtAt
-      const ticketBoughtAt = await this.weeklyLottery.ticketBoughtAt(
+      // ticketReceivedAt
+      const ticketReceivedAt = await this.weeklyLottery.ticketReceivedAt(
+        index,
         ticketLastId
       );
-      expect(ticketBoughtAt <= new Date().getTime()).to.equal(true);
+      expect(ticketReceivedAt <= new Date().getTime()).to.equal(true);
     });
 
     it("Three user should buy ticket", async function () {
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -245,48 +253,48 @@ describe("TokenTimedRandomSendContract", function () {
       );
 
       // ticketLastIdが追加されていること
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       expect(ticketLastId).to.equal("3");
 
       // ticketLastNumberがあること
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       expect(ticketLastNumber).to.equal("9");
 
       // ticketCountが追加されていること
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       expect(ticketCount).to.equal("3");
 
       // isParticipatedがtrueに更新されていること
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         user.address
       );
       expect(isParticipated).to.equal(true);
 
       // participantCountが更新されていること
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       expect(participantCount).to.equal("3");
 
       // ticketIds
-      const ticketIds = await this.weeklyLottery.getTicketIds(user.address);
+      const ticketIds = await this.weeklyLottery.ticketIds(index, user.address);
       expect(ticketIds[0]).to.equal("3");
 
-      // ticketOwner
-      const ticketOwner = await this.weeklyLottery.ticketOwner(ticketLastId);
-      expect(ticketOwner).to.equal(user.address);
-
-      // ticketBoughtAt
-      const ticketBoughtAt = await this.weeklyLottery.ticketBoughtAt(
+      // ticketReceivedAt
+      const ticketReceivedAt = await this.weeklyLottery.ticketReceivedAt(
+        index,
         ticketLastId
       );
-      expect(ticketBoughtAt <= new Date().getTime()).to.equal(true);
+      expect(ticketReceivedAt <= new Date().getTime()).to.equal(true);
     });
 
     it("The same user buys ticket again.", async function () {
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -325,62 +333,65 @@ describe("TokenTimedRandomSendContract", function () {
       );
 
       // ticketLastIdが追加されていること
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       expect(ticketLastId).to.equal("4");
 
       // ticketLastNumberがあること
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       expect(ticketLastNumber).to.equal("12");
 
       // ticketCountが追加されていること
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       expect(ticketCount).to.equal("3");
 
       // isParticipatedがtrueに更新されていること
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         user.address
       );
       expect(isParticipated).to.equal(true);
 
       // participantCountが更新されていること
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       expect(participantCount).to.equal("3");
 
       // ticketIds
-      let ticketIds = await this.weeklyLottery.getTicketIds(
+      let ticketIds = await this.weeklyLottery.ticketIds(
+        index,
         this.signers[0].address
       );
       expect(ticketIds[0]).to.equal("1");
       expect(ticketIds[1]).to.equal("4");
 
-      ticketIds = await this.weeklyLottery.getTicketIds(
+      ticketIds = await this.weeklyLottery.ticketIds(
+        index,
         this.signers[1].address
       );
       expect(ticketIds[0]).to.equal("2");
 
-      ticketIds = await this.weeklyLottery.getTicketIds(
+      ticketIds = await this.weeklyLottery.ticketIds(
+        index,
         this.signers[2].address
       );
       expect(ticketIds[0]).to.equal("3");
 
-      // ticketOwner
-      const ticketOwner = await this.weeklyLottery.ticketOwner(ticketLastId);
-      expect(ticketOwner).to.equal(user.address);
-
-      // ticketBoughtAt
-      const ticketBoughtAt = await this.weeklyLottery.ticketBoughtAt(
+      // ticketReceivedAt
+      const ticketReceivedAt = await this.weeklyLottery.ticketReceivedAt(
+        index,
         ticketLastId
       );
-      expect(ticketBoughtAt <= new Date().getTime()).to.equal(true);
+      expect(ticketReceivedAt <= new Date().getTime()).to.equal(true);
     });
 
     it("Owner should send ticket", async function () {
       const user = this.signers[0];
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -394,13 +405,65 @@ describe("TokenTimedRandomSendContract", function () {
 
       await this.weeklyLottery.sendTicket(0, this.signers[1].address);
       // ticketIds
-      // let ticketIds = await this.weeklyLottery.getTicketIds(
-      //   this.signers[0].address
-      // );
-      // console.log(ticketIds);
+      let ticketIds = await this.weeklyLottery.ticketIds(
+        index,
+        this.signers[0].address
+      );
+      expect(ticketIds.length).to.equal(0);
 
-      // ticketIds = await this.weeklyLottery.getTicketIds(this.signers[1].address);
-      // console.log(ticketIds);
+      ticketIds = await this.weeklyLottery.ticketIds(
+        index,
+        this.signers[1].address
+      );
+      expect(ticketIds[0]).to.equal("1");
+      expect(ticketIds.length).to.equal(1);
+
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
+      expect(ticketLastId).to.equal("1");
+
+      // ticketLastNumberがあること
+      const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
+        ticketLastId.toString()
+      );
+      expect(ticketLastNumber).to.equal("3");
+
+      // ticketCountが追加されていること
+      const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
+        ticketLastId.toString()
+      );
+      expect(ticketCount).to.equal("3");
+
+      // isParticipatedがtrueに更新されていること
+      const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
+        this.signers[1].address
+      );
+      expect(isParticipated).to.equal(true);
+
+      // participantCountが更新されていること
+      const participantCount = await this.weeklyLottery.participantCount(index);
+      expect(participantCount).to.equal("2");
+    });
+
+    it("Non-Owner can not send ticket", async function () {
+      const user = this.signers[1];
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
+      const _ticketCount = 3;
+      const tokenAmount = String(ticketPrice * _ticketCount);
+
+      await mintAndBuyTicket(
+        this.lotteryERC20,
+        this.weeklyLottery,
+        user,
+        tokenAmount,
+        _ticketCount
+      );
+
+      await expect(
+        this.weeklyLottery.connect(user).sendTicket(0, this.signers[2].address)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
     });
   });
 
@@ -442,19 +505,22 @@ describe("TokenTimedRandomSendContract", function () {
       const name = await this.weeklyLottery.name();
       const symbol = await this.weeklyLottery.symbol();
       const cycleTimestamp = await this.weeklyLottery.cycleTimestamp();
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const keyHash = await this.weeklyLottery.keyHash();
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         this.signers[0].address
       );
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       const isOnlyOwner = await this.weeklyLottery.isOnlyOwner();
 
       expect(name).to.equal("weeklyLottery");
@@ -473,7 +539,7 @@ describe("TokenTimedRandomSendContract", function () {
 
     it("Owner user can buy ticket", async function () {
       const user = this.signers[0];
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -486,48 +552,48 @@ describe("TokenTimedRandomSendContract", function () {
       );
 
       // ticketLastIdが追加されていること
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       expect(ticketLastId).to.equal("1");
 
       // ticketLastNumberがあること
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       expect(ticketLastNumber).to.equal("3");
 
       // ticketCountが追加されていること
       const ticketCount = await this.weeklyLottery.ticketCount(
+        index,
         ticketLastId.toString()
       );
       expect(ticketCount).to.equal("3");
 
       // isParticipatedがtrueに更新されていること
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         user.address
       );
       expect(isParticipated).to.equal(true);
 
       // participantCountが更新されていること
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       expect(participantCount).to.equal("1");
 
       // ticketIds
-      const ticketIds = await this.weeklyLottery.getTicketIds(user.address);
+      const ticketIds = await this.weeklyLottery.ticketIds(index, user.address);
       expect(ticketIds[0]).to.equal("1");
 
-      // ticketOwner
-      const ticketOwner = await this.weeklyLottery.ticketOwner(ticketLastId);
-      expect(ticketOwner).to.equal(user.address);
-
-      // ticketBoughtAt
-      const ticketBoughtAt = await this.weeklyLottery.ticketBoughtAt(
+      // ticketReceivedAt
+      const ticketReceivedAt = await this.weeklyLottery.ticketReceivedAt(
+        index,
         ticketLastId
       );
-      expect(ticketBoughtAt <= new Date().getTime()).to.equal(true);
+      expect(ticketReceivedAt <= new Date().getTime()).to.equal(true);
     });
 
     it("Non-Owner can not buy ticket", async function () {
-      const ticketPrice = await this.weeklyLottery.ticketPrice();
+      const ticketPrice = await this.weeklyLottery.ticketPrice(index);
       const _ticketCount = 3;
       const tokenAmount = String(ticketPrice * _ticketCount);
 
@@ -551,36 +617,32 @@ describe("TokenTimedRandomSendContract", function () {
         this.weeklyLottery.connect(user).buyTicket(_ticketCount)
       ).to.be.revertedWith("Ownable: caller is not the owner");
       // ticketLastIdが追加されてないこと
-      const ticketLastId = await this.weeklyLottery.ticketLastId();
+      const ticketLastId = await this.weeklyLottery.ticketLastId(index);
       expect(ticketLastId).to.equal("1");
 
       // ticketLastNumberが追加されてないこと
       const ticketLastNumber = await this.weeklyLottery.ticketLastNumber(
+        index,
         ticketLastId.toString()
       );
       expect(ticketLastNumber).to.equal("3");
 
-      const ticketCount = await this.weeklyLottery.ticketCount("2");
+      const ticketCount = await this.weeklyLottery.ticketCount(index, "2");
       expect(ticketCount).to.equal("0");
 
       const isParticipated = await this.weeklyLottery.isParticipated(
+        index,
         user.address
       );
       expect(isParticipated).to.equal(false);
 
       // participantCountが更新されていること
-      const participantCount = await this.weeklyLottery.participantCount();
+      const participantCount = await this.weeklyLottery.participantCount(index);
       expect(participantCount).to.equal("1");
 
       // ticketIds
-      const ticketIds = await this.weeklyLottery.getTicketIds(user.address);
+      const ticketIds = await this.weeklyLottery.ticketIds(index, user.address);
       expect(ticketIds.length).to.equal(0);
-
-      // ticketOwner
-      const ticketOwner = await this.weeklyLottery.ticketOwner("2");
-      expect(ticketOwner).to.equal(
-        "0x0000000000000000000000000000000000000000"
-      );
     });
   });
 });
